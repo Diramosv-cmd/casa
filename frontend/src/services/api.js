8d6f7d0fd75253707d66544597e22fd12912c7e8
Diego Ramos <diramosv@hotmail.com>
2026-05-04 17:40:38 -0400

    Add explicit bcrypt dependency

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://${window.location.hostname}:8000`,
+  baseURL: 'https://casa-kih7.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
